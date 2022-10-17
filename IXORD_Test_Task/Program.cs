3fedd6435bbbfe21c73f3d84e7143587ddba1008
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-10-17 14:27:59 +0300

    Add project files.

---
+++ IXORD_Test_Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IXORD_Test_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
